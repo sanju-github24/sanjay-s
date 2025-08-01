6cee41b3637e1b6a7a438f2074bacde768443b06
sanjay <sanjusanjay0444@gmail.com>
2025-08-01 11:29:58 +0530

    good

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const aboutItems = [
         <div className="container">
           <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up">
             <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
-              Welcome! I&apos;m Henry, a professional web developer with a knack for crafting visually stunning and highly functional websites. Combining creativity and technical expertise, I transform your vision into a digital masterpiece that excels in both appearance and performance.
+            Welcome! I'm Sanjay, a 2nd-year B.Tech student passionate about web development. I specialize in building clean, responsive, and user-friendly websites that not only look great but also deliver smooth performance. With a growing skillset and a strong eye for design and functionality, I turn ideas into impactful digital experiences.
             </p>
   
             <div className="flex flex-wrap items-center gap-4 md:gap-7">
